cdc9686cfb7c4e406664b645b6b1080709af2821
ha quang trung <haquangtrungceo@gmail.com>
2018-06-04 10:38:48 +0700

    add project

---
+++ app/src/main/java/admin/trunghq/useinputcontrols/CheckBox.java
@@ -0,0 +1,47 @@
+package admin.trunghq.useinputcontrols;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * @author Hà Quang Trung
+ * @version 1.0.0
+ * @description
+ * @desc Technical Director, FPT-Software.
+ * @created ${Date}
+ * @updated ${Date}
+ * @modified by
+ * @updated on ${Date}
+ * @since 1.0
+ */
+public class CheckBox extends AppCompatActivity {
+
+    // HQT
+    /*CheckBox chocolate;
+    CheckBox sprinkles;
+    CheckBox nuts;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.check_box);
+        chocolate = findViewById(R.id.checkbox1_chocolate);
+        findViewById(R.id.btnSubmit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringBuffer toppings = new StringBuffer().append("hello");
+                if ((findViewById(R.id.checkbox1_chocolate)).is()) {
+                    toppings.append(getString(R.string.chocolate_syrup_text));
+                }
+                if (((CheckBox) findViewById(R.id.checkbox2_sprinkles)).isChecked()) {
+                    toppings.append(getString(R.string.sprinkles_text));
+                }
+                if (((CheckBox) findViewById(R.id.checkbox3_nuts)).isChecked()) {
+                    toppings.append(getString(R.string.crushed_nuts_text));
+                }
+            }
+        });
+
+    }*/
+}
---
+++ app/src/main/java/admin/trunghq/useinputcontrols/MainActivity.java
@@ -0,0 +1,19 @@
+package admin.trunghq.useinputcontrols;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * The type Main activity.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
